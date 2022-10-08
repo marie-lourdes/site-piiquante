1851d9fed7b848fbcbb5fb17e9f46b4f69b3781a
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-10-08 20:22:06 +0200

    creation server node with and module app.js with framework express, configuration variable environment PORT with dotenv

--- back-end/app.js
+++ back-end/app.js
@@ -1,4 +1,13 @@
+// module app.js qui recupere le module express et execute l application ainsi que ces fonctions middleware qui traite les requête du serveur crée dans server.js
 const express = require( "express" );
+
+//on execute l application express via sa methode express() accessible grace au module express importé
 const app = express();
 
-module.exports= app;
+app.use( ( req, res )=>{
+    console.log("requete bien recue")
+    res.send( "requête reçue" );
+});
+
+// exporte la valeur actuelle de l objet exports du module app.js pour le rendre accessible hors de ce module, notamment au fichier server.js, pour que le serveur node s execute avec express et les fonctionnalités de l application express
+module.exports = app;
--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,16 @@
+// création du serveur avec le module http et  le module app.js qui exécute l ' application express
 const http = require( "http" );
-const app = require( "./app" )
-
+const app = require( "./app" );
+/*recupération du module dotenv pour charger la variable d'environnement PORT defini dans le fichier .env
+pour eviter les changements d adresse ip et de port, le port ne prendra pas la valeur du port par defaut du systeme d exploitation qui est aleatoire et qui peut changer,
+ on a redefini cette valeur par defaut de cette variable d environnemnt PORT du systeme d exploitation window en la specifiant et en chargeant cette variable d environnement avec dotenv */
 const dotenv = require("dotenv");
 dotenv.config();
+// redefinition du port par defaut du systeme d exploitation
+const MY_PORT = process.env.PORT;
+
+
+//creation du serveur avec le module http et en appelant les fonctions et methodes du module apps.js
+const server = http.createServer(app);
+//le serveur attend les requete sur ce port et ecoute sur port, requete qui sera traité par l aplication express qui est appélé dans le module app.js
+server.listen(MY_PORT);
