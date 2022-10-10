402c3606a6613740580abf3faf6a78a1a31fcf9c
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-10-10 11:11:18 +0200

    configuration  CORS objt response for all reequest API and all root of API

--- back-end/app.js
+++ back-end/app.js
@@ -5,7 +5,15 @@ const express = require( "express" );
 //on execute l application express via sa methode express() accessible grace au module express importé
 const app = express();
 
-
+// .........................CONFIGURATION GENERALE DES HEADERS RESPONSE ET DE L'ACCES AUX RESSOURCES DE L API POUR TOUS  DE REQUETES DU FRONT END VERS L API............................
+// configuration generale de la reponse retourné (objet response) suite à une requete du front end avec la modification de la securite d acces au ressource de CORS
+// en ajoutant des headers à l objet response pour tout type de requete et sur toutes les routes
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 app.use( ( req, res )=>{
     console.log("requete bien recue")
