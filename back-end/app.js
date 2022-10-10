4d3248d43db47daa276575731a68800ac85f28a8
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-10-10 11:41:34 +0200

    configuration request headers content-type json width method express.json

--- back-end/app.js
+++ back-end/app.js
@@ -1,12 +1,20 @@
-//.................creation de l application express......................
+//..........................CREATION DE L APPLICATION EXPRESS......................
+
 // module app.js qui recupere le module express et execute l application ainsi que ces fonctions middleware qui traite les requête du serveur crée dans server.js
 const express = require( "express" );
 
 //on execute l application express via sa methode express() accessible grace au module express importé
 const app = express();
+//........................CONFIGURATION GENERALE DES REQUETES ENTRANTES ET DE L'OBJET REQUEST DONT LE BODY EST INDIQUÉ  AU FORMAT JSON DANS LE HEADER DE LA REQUETE...................
 
-// .........................CONFIGURATION GENERALE DES HEADERS RESPONSE ET DE L'ACCES AUX RESSOURCES DE L API POUR TOUS  DE REQUETES DU FRONT END VERS L API............................
-// configuration generale de la reponse retourné (objet response) suite à une requete du front end avec la modification de la securite d acces au ressource de CORS
+// ajout d un middleware integrée a express qui va recuperer toutes les requêtes entrante (objet request) dont le body est en content-type: application json
+// ce middleware ne doit etre placer dans le tunnel gestionnaire des middleware avec le chainage de next(), 
+// ce middleware traite et analyse  l'objet request des requete entrante en json avec la methode express.json()
+app.use(express.json());
+
+// .........................CONFIGURATION GENERALE DES HEADERS RESPONSE ET DE L'ACCES AUX RESSOURCES DE L API POUR TOUS  DE REQUETES DU FRONT END VERS L API..........................
+
+// configuration generale CORS de la reponse retourné (objet response) suite à une requete du front end avec la modification de la securite d acces au ressource de CORS
 // en ajoutant des headers à l objet response pour tout type de requete et sur toutes les routes
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
