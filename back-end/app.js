187aa19eefd0ed10737fe30b632176addc78f3c3
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-10-10 11:07:56 +0200

    fincish branch

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,11 @@
+//.................creation de l application express......................
 // module app.js qui recupere le module express et execute l application ainsi que ces fonctions middleware qui traite les requête du serveur crée dans server.js
 const express = require( "express" );
 
 //on execute l application express via sa methode express() accessible grace au module express importé
 const app = express();
+
+
 
 app.use( ( req, res )=>{
     console.log("requete bien recue")
