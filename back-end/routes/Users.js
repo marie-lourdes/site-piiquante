aa153668287b03e5333eb189fea4123797a0cd40
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-10-15 09:00:05 +0200

    routes Users:create route signup route login

--- back-end/routes/Users.js
+++ back-end/routes/Users.js
@@ -1,3 +1,13 @@
 const express = require( "express" );
 // On crée un objet router pour y ajouter les routes individuelles  qui seront rajouté à un route de base, une route principale
 const router = express.Router()
+
+//..................CRÉATION DES ROUTES POUR L INSCRIPTION D'UN UTILISATEUR ET LA CONNEXION D'UN UTILISATEUR DEJA INSCRIT.................
+
+router.post( "/signup", ( req, res ) => {
+
+});
+
+router.post( "/login", ( req, res ) => {
+
+});
