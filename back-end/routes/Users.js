344810bfe3b871157c95749f2d543675446831d3
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-10-15 08:51:41 +0200

    add file routes Users.js

---
+++ back-end/routes/Users.js
@@ -0,0 +1,3 @@
+const express = require( "express" );
+// On crée un objet router pour y ajouter les routes individuelles  qui seront rajouté à un route de base, une route principale
+const router = express.Router()
